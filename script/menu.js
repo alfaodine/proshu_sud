95ab07e59db4b200b32671fabb524013df86f2c0
Mariia Bezditna <mariiabezditna@MacBook-Air-Mariia.local>
2022-03-06 21:30:19 +0200

    updated mobile menu

--- script/menu.js
+++ script/menu.js
@@ -1,15 +1,25 @@
 let nav = document.querySelector('.nav-toggle');
 let menu = document.querySelector('.header__menu');
+let menuBg = document.querySelector('.header__menu-bg');
 let presonalArea = document.querySelector('.header__personal-area');
 
 nav.addEventListener('click', function(){
+    // menuBg.setAttribute("style","height:500px");
+    // console.log(menu.clientHeight);
     nav.classList.toggle('opened');
+    menuBg.classList.toggle('active-menu');
     if (nav.classList.contains('opened')){
-        menu.setAttribute('style', 'left: 0%');
-        presonalArea.setAttribute('style', 'left: 0%');
+        
+        // menuBg.classList.toggle('active-menu');
+
+        // menu.setAttribute('style', 'left: 0%');
+        // menuBg.setAttribute('style', `left: 0%; top: 7vh; height:${menu.clientHeight}px`);
+        // presonalArea.setAttribute('style', 'left: 0%');
 
     }else{
-        menu.setAttribute('style', 'left: -105%');
-        presonalArea.setAttribute('style', 'left: -105%');
+        // menu.setAttribute('style', 'left: -100%');
+        // presonalArea.setAttribute('style', 'left: -100%');
+        // menuBg.setAttribute('style', 'top: -50vh; left: -105%');
+        // menuBg.classList.toggle('active-menu');
     }
 })
