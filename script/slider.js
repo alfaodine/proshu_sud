dbad78ac6ea2e0c0557daa495a21b6cbc951beb2
alfaodine <yatskevych.andrii@gmail.com>
2022-03-08 20:07:11 +0200

    mobile menu final

--- script/slider.js
+++ script/slider.js
@@ -9,6 +9,21 @@ $(document).ready(function(){
   slidesToShow: 3,
   slidesToScroll: 1,
   prevArrow: $('.prev-btn'),
-  nextArrow: $('.next-btn')
+  nextArrow: $('.next-btn'),
+  
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
     });
   });
